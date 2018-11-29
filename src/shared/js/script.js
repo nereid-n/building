04528bf12480a798f1686bb1cd93b71ba171e7d0
nereid <nas.kuleshova@gmail.com>
2018-11-29 13:32:56 +0300

    add modal

--- src/shared/js/script.js
+++ src/shared/js/script.js
@@ -1,3 +1,25 @@
-class Shared {
-    constructor() {}
+if (document.querySelector('.overlay')) {
+  var overlay = document.querySelector('.overlay');
+  overlay.onclick = modalClose;
+}
+
+if(document.querySelector('.js-modal-btn')) {
+  let btns = document.querySelectorAll('.js-modal-btn');
+  for (let i = 0; i < btns.length; i++) {
+    let btn = btns[i];
+    btn.onclick = function() {
+      let id = btn.dataset.id;
+      let modal = document.getElementById(id);
+      modal.classList.add('modal-active');
+      overlay.classList.add('overlay-active');
+    }
+  }
+}
+
+if (document.querySelector('.js-btn-close')) {
+  let btnModalClose = document.querySelectorAll('.js-btn-close');
+  for (let i = 0; i < btnModalClose.length; i++) {
+    let btn = btnModalClose[i];
+    btn.onclick = modalClose;
+  }
 }
